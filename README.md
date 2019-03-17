db9a83117af51be8dff63a92360dd04e4d8ceef1
Tensai <sudarshan.parvatikar@gmail.com>
2019-03-17 22:19:51 +0530

    added lukman's code and folder

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # kyonen-no-project
 The final year project
+
+
+# Resources
+1. https://www.coursera.org/lecture/dsp/5-8-multirate-signal-processing-vdsuP
---
+++ multi.py
@@ -0,0 +1,262 @@
+import sys
+import fractions
+import numpy
+from scipy import signal
+from numpy import cos, sin, pi, absolute, arange, asarray, array, log10
+# from sp import multirate4
+from scipy.signal import kaiserord, lfilter, firwin, freqz, iirfilter, freqs, butter
+from pylab import figure, clf, plot, xlabel, ylabel, xlim, ylim, title, grid, axes, show, legend
+from itertools import zip_longest
+from random import randint as ri
+
+
+sample_rate = 44000
+nsamples = 4 * int(sample_rate)
+t = arange(nsamples) / int(sample_rate)
+original = cos(2*pi*0.5*t) + 0.2*sin(2*pi*2.5*t+0.1) + \
+        0.2*sin(2*pi*15.3*t) + 0.1*sin(2*pi*16.7*t + 0.1) + \
+            0.1*sin(2*pi*23.45*t+.8)
+
+
+# noise = numpy.random.uniform(low=0, high=10, size=(int(len(t)/ri(1, 3000)),))
+# corrupted_signal = [x + y for x, y in zip_longest(list(original), list(noise), fillvalue=0)]
+# print(">>>>>>>>>>>>>>", len(original), len(corrupted_signal))
+x = original[:len(t)]
+print(x)
+
+
+
+# The Nyquist rate of the signal.
+nyq_rate = sample_rate / 2.0
+
+# The desired width of the transition from pass to stop,
+# relative to the Nyquist rate.  We'll design the filter
+# with a 5 Hz transition width.
+width = 5.0/nyq_rate
+
+# The desired attenuation in the stop band, in dB.
+ripple_db = 60.0
+
+#------------------------------------------------------------------------------------------
+
+# Create FIR Filter and apply to x
+
+# Compute the order and Kaiser parameter for the FIR filter.
+N, beta = kaiserord(ripple_db, width)
+
+# The cutoff frequency of the filter.
+# cutoff_hz = 10.0
+low = 20.0
+high = 20000.0
+
+
+#------------------------------------------------------------------------------------------
+
+# Multirate block
+
+# -----------------------------------------------------
+
+#####################################################
+
+def downsample(s, n, phase=0):
+    """Decrease sampling rate by integer factor n with included offset phase.
+    """
+    n = int(n)
+    return s[phase::n]
+
+
+
+def upsample(s, n, phase=0):
+    """Increase sampling rate by integer factor n  with included offset phase.
+    """
+    return numpy.roll(numpy.kron(s, numpy.r_[1, numpy.zeros(int(n)-1)]), phase)
+
+
+
+def decimate(s, r, n=None, fir=False):
+    """Decimation - decrease sampling rate by r. The decimation process filters 
+    the input data s with an order n lowpass filter and then resamples the 
+    resulting smoothed signal at a lower rate. By default, decimate employs an 
+    eighth-order lowpass Chebyshev Type I filter with a cutoff frequency of 
+    0.8/r. It filters the input sequence in both the forward and reverse 
+    directions to remove all phase distortion, effectively doubling the filter 
+    order. If 'fir' is set to True decimate uses an order 30 FIR filter (by 
+    default otherwise n), instead of the Chebyshev IIR filter. Here decimate 
+    filters the input sequence in only one direction. This technique conserves 
+    memory and is useful for working with long sequences.
+    """
+    if fir:
+        if n is None:
+            n = 30
+        b = signal.firwin(n, 1.0/r)
+        a = 1
+        f = signal.lfilter(b, a, s)
+    else: #iir
+        if n is None:
+            n = 8
+        b, a = signal.cheby1(n, 0.05, 0.8/r)
+        f = signal.filtfilt(b, a, s)
+    return downsample(f, r)
+
+
+
+def interp(s, r, l=4, alpha=0.5):
+    """Interpolation - increase sampling rate by integer factor r. Interpolation 
+    increases the original sampling rate for a sequence to a higher rate. interp
+    performs lowpass interpolation by inserting zeros into the original sequence
+    and then applying a special lowpass filter. l specifies the filter length 
+    and alpha the cut-off frequency. The length of the FIR lowpass interpolating
+    filter is 2*l*r+1. The number of original sample values used for 
+    interpolation is 2*l. Ordinarily, l should be less than or equal to 10. The 
+    original signal is assumed to be band limited with normalized cutoff 
+    frequency 0=alpha=1, where 1 is half the original sampling frequency (the 
+    Nyquist frequency). The default value for l is 4 and the default value for 
+    alpha is 0.5.
+    """
+    b = signal.firwin(2*l*r+1, alpha/r);
+    a = 1
+    return r*signal.lfilter(b, a, upsample(s, r))[r*l+1:-1]
+
+
+def resample(s, p, q, h=None):
+    """Change sampling rate by rational factor. This implementation is based on
+    the Octave implementation of the resample function. It designs the 
+    anti-aliasing filter using the window approach applying a Kaiser window with
+    the beta term calculated as specified by [2].
+    
+    Ref [1] J. G. Proakis and D. G. Manolakis,
+    Digital Signal Processing: Principles, Algorithms, and Applications,
+    4th ed., Prentice Hall, 2007. Chap. 6
+
+    Ref [2] A. V. Oppenheim, R. W. Schafer and J. R. Buck, 
+    Discrete-time signal processing, Signal processing series,
+    Prentice-Hall, 1999
+    """
+    gcd = fractions.gcd(p,q)
+    if gcd>1:
+        p=p/gcd
+        q=q/gcd
+    
+    if h is None: #design filter
+        #properties of the antialiasing filter
+        log10_rejection = -3.0
+        stopband_cutoff_f = 1.0/(2.0 * max(p,q))
+        roll_off_width = stopband_cutoff_f / 10.0
+    
+        #determine filter length
+        #use empirical formula from [2] Chap 7, Eq. (7.63) p 476
+        rejection_db = -20.0*log10_rejection;
+        l = numpy.ceil((rejection_db-8.0) / (28.714 * roll_off_width))
+  
+        #ideal sinc filter
+        t = numpy.arange(-l, l + 1)
+        ideal_filter=2*p*stopband_cutoff_f*numpy.sinc(2*stopband_cutoff_f*t)  
+  
+        #determine parameter of Kaiser window
+        #use empirical formula from [2] Chap 7, Eq. (7.62) p 474
+        beta = signal.kaiser_beta(rejection_db)
+          
+        #apodize ideal filter response
+        h = numpy.kaiser(2*l+1, beta)*ideal_filter
+
+    ls = len(s)
+    lh = len(h)
+
+    l = (lh - 1)/2.0
+    ly = numpy.ceil(ls*p/float(q))
+
+    #pre and postpad filter response
+    nz_pre = numpy.floor(q - numpy.mod(l,q))
+    nz_pre = int(nz_pre)
+    hpad = h[-lh+nz_pre:]
+
+    offset = numpy.floor((l+nz_pre)/q)
+    nz_post = 0;
+    while numpy.ceil(((ls-1)*p + nz_pre + lh + nz_post )/q ) - offset < ly:
+        nz_post += 1
+    hpad = hpad[:lh + nz_pre + nz_post]
+
+    #filtering
+    xfilt = upfirdn(s, hpad, p, q)
+
+    return xfilt[int(offset)-1:int(offset)-1+int(ly)]
+
+
+def upfirdn(s, h, p, q):
+    """Upsample signal s by p, apply FIR filter as specified by h, and 
+    downsample by q. Using fftconvolve as opposed to lfilter as it does not seem
+    to do a full convolution operation (and its much faster than convolve).
+    """
+    return downsample(signal.fftconvolve(h, upsample(s, p)), q)
+
+##################################################################################
+
+
+
+### Low Pass Filter Block
+
+
+def lowPassFilter(input_signal, order, nyq_rate):
+	# Create a low pass filter with cut off frequency 20k Hz -> pass all freq below 20k Hz
+	# The cutoff frequency of the filter.
+	cutoff_hz = 20000.0
+
+	# Use firwin with a Kaiser window to create a lowpass FIR filter.
+	taps = firwin(order, cutoff_hz/nyq_rate, window=('kaiser', beta))
+
+	# Use lfilter to filter x with the FIR filter.
+	filtered_x = lfilter(taps, 1.0, input_signal)
+
+	return filtered_x
+
+#### /Low Pass Filter Block
+
+### High Pass Filter Block
+
+def highPassFilter(input_signal, order, nyq_rate):
+	# Create a High pass filter, with cut off freq 20 Hz -> pass all freq above 20k Hz
+	# The cutoff frequency of the filter.
+	cutoff_hz = 20.0
+
+	# Use firwin with a Kaiser window to create a lowpass FIR filter.
+	taps = firwin(order, cutoff_hz/nyq_rate, window=('kaiser', beta), pass_zero=False)
+
+	# Use lfilter to filter x with the FIR filter.
+	filtered_x = lfilter(taps, 1.0, input_signal)
+
+	return filtered_x
+
+### /High Pass Filter Block
+# ------------------------------------------------------
+
+# 1. Pass Signal via the low pass filter and get output
+
+filtered_signal = lowPassFilter(x, N, nyq_rate)
+
+print(filtered_signal)
+
+# 2. Down Sample the flitered signal
+
+# 3. Upsample the filtered signal
+
+# 4. Pass signal via high pass filter and get output
+
+filtered_signal = lowPassFilter(x, N, nyq_rate)
+print(filtered_signal)
+
+# 5. Add the signals to get output signals
+
+delay = 0.5 * (N-1) / sample_rate
+
+figure(1)
+# Plot the original signal.
+plot(t, original, label='original signal')
+# Plot the filtered signal, shifted to compensate for the phase delay.
+# plot(t-delay, filtered_x, 'r-', label='shifted signal')
+# Plot just the "good" part of the filtered signal.  The first N-1
+# samples are "corrupted" by the initial conditions.
+# plot(t[N-1:]-delay, filtered_signal[N-1:], 'g', linewidth=4, label='filtered signal')
+legend()
+xlabel('t')
+grid(True)
+show()
